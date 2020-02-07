7fdc6b1aae698fadfdaf8f8cd6e9811e36ec4f81
Dominik Robert <48982253+Dominik-Robert@users.noreply.github.com>
2020-02-07 11:21:43 +0100

    Update exit_code_tester command.cmd
    All field values must be string

--- scripts/exit_code_tester command.cmd
+++ scripts/exit_code_tester command.cmd
@@ -1,4 +1,4 @@
 @echo off
-echo Testet Commands %1 %2 %3 %4 {{"counter":7, "temporary": "hello World"}}
+echo Testet Commands %1 %2 %3 %4 {{"counter":"7", "temporary": "hello World"}}
 @ping localhost -n 3 > NUL
 exit /B 0
